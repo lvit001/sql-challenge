bc62b0ef3039138a831fc5f0f2ae8a634f720956
lvit001 <140283164+lvit001@users.noreply.github.com>
2023-12-02 10:45:53 -0600

    Added code for question 7

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -64,9 +64,16 @@ WHERE emp_no IN
 	)
 );
 
+-- 7) List each employee in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
 
-
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp as de
+JOIN employees as e ON
+de.emp_no=e.emp_no
+JOIN departments as d ON
+de.dept_no=d.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
 
 
 
